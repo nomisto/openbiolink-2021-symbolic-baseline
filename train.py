95f5758cf62fe1244bef4d1ae8f2a9ff73a2a0c6
nomisto <simonott@gmx.at>
2021-09-02 10:20:20 +0200

    Updated for new evaluator

--- train.py
+++ train.py
@@ -2,27 +2,33 @@ import subprocess
 import os
 from openbiolink.obl2021 import OBL2021Dataset
 
-working_dir = os.path.abspath('obl2021').replace("\\","/")
+def main():
 
-print(f"Working directory is {working_dir}")
+    working_dir = os.path.abspath('obl2021').replace("\\","/")
 
-dl = OBL2021Dataset()
+    print(f"Working directory is {working_dir}")
 
-with open(os.path.join(working_dir, "learn_.txt"),"w") as outfile:
-    outfile.write(f"PATH_TRAINING  = {working_dir}/train.tsv" + "\n")
-    outfile.write(f"PATH_OUTPUT     = {working_dir}/rules" + "\n")
-    outfile.write(f"SNAPSHOTS_AT     = 2000" + "\n")
-    outfile.write(f"WORKER_THREADS      = 15" + "\n")
+    dl = OBL2021Dataset()
 
-subprocess.call([r"java", "-cp", "./AnyBURL-RE/AnyBURL-RE.jar", "de.unima.ki.anyburl.LearnReinforced", os.path.join(working_dir, "learn_.txt")])
-
-
-with open(os.path.join(working_dir, "apply_.txt"),"w") as outfile:
+    with open(os.path.join(working_dir, "learn_.txt"),"w") as outfile:
         outfile.write(f"PATH_TRAINING  = {working_dir}/train.tsv" + "\n")
-        outfile.write(f"PATH_TEST      = {working_dir}/test.tsv" + "\n")
-        outfile.write(f"PATH_VALID     = {working_dir}/valid.tsv" + "\n")
-        outfile.write(f"PATH_RULES     = {working_dir}/rules-2000" + "\n")
-        outfile.write(f"PATH_OUTPUT = {working_dir}/prediction" + "\n")
-        outfile.write(f"TOP_K_OUTPUT = 10" + "\n")
+        outfile.write(f"PATH_OUTPUT     = {working_dir}/rules" + "\n")
+        outfile.write(f"SNAPSHOTS_AT     = 2000" + "\n")
+        outfile.write(f"WORKER_THREADS      = 15" + "\n")
 
-subprocess.call([r"./SAFRAN/build/Release/SAFRAN", "applymax", os.path.join(working_dir, "apply_.txt")])
+    subprocess.call([r"java", "-cp", "./AnyBURL-RE/AnyBURL-RE.jar", "de.unima.ki.anyburl.LearnReinforced", os.path.join(working_dir, "learn_.txt")])
+
+
+    with open(os.path.join(working_dir, "apply_.txt"),"w") as outfile:
+            outfile.write(f"PATH_TRAINING  = {working_dir}/train.tsv" + "\n")
+            outfile.write(f"PATH_TEST      = {working_dir}/test.tsv" + "\n")
+            outfile.write(f"PATH_VALID     = {working_dir}/valid.tsv" + "\n")
+            outfile.write(f"PATH_RULES     = {working_dir}/rules-2000" + "\n")
+            outfile.write(f"PATH_OUTPUT = {working_dir}/prediction" + "\n")
+            outfile.write(f"TOP_K_OUTPUT = 10" + "\n")
+
+    subprocess.call([r"./SAFRAN/build/Release/SAFRAN", "applymax", os.path.join(working_dir, "apply_.txt")])
+    
+    
+if __name__ == "__main__":
+    main()
